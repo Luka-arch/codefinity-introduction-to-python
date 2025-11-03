d8fd25f08f3e13aabfe8dd8245d0ffeda905bb27
Luka-arch <>
2025-11-03 17:06:35 +0000

    Variable Naming

--- variables_and_types/variable_naming/main.py
+++ variables_and_types/variable_naming/main.py
@@ -2,6 +2,11 @@
 
 
 # Testing
-print("Item:", ___)
-print("Price per loaf: $", ___)
-print("Quantity in stock:", ___)
+item_name = "Bread"
+print(item_name)
+
+item_price = 4.52
+print(item_price)
+
+item_quantity = 230
+print(item_quantity)
